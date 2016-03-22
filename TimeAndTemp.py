c138efc977531efc7569145e208c8636d61821c1
Cody Jarvis <cody.c.jarvis@gmail.com>
2016-03-21 22:10:10 -0500

    Added weather function

--- TimeAndTemp.py
+++ TimeAndTemp.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from twilio import twiml
 from datetime import datetime
+import xml.etree.ElementTree as ET
+import urllib2
+
 
 app = Flask(__name__)
 
@@ -9,8 +12,12 @@ app = Flask(__name__)
 def voice_response():
     """Respond to incoming requests."""
     resp = twiml.Response()
+    # give time
     resp.say(get_datetime())
-
+    # pause 3 seconds between time and temp
+    resp.pause(3)
+    # give temp
+    resp.say(get_weather())
     return str(resp)
 
 
@@ -29,12 +36,25 @@ def get_datetime():
         period = 'a m'
         pass
 
-    time_srt = 'Today is {} {}. The current time is {} {} {}'.format(month, day, hour, minute, period)
+    time_str = 'Today is {} {}. The current time is {} {} {}. '.format(month, day, hour, minute, period)
 
-    return str(time_srt)
+    return str(time_str)
 
 def get_weather():
-    pass
+    """Get and build out weather string. Uses NOAA XML."""
+    # gets weather xml from noaa web. should replace KLBF with local call site.
+    weather_xml = urllib2.urlopen('http://w1.weather.gov/xml/current_obs/KLBF.xml')
+    # open xml
+    weather_tree = ET.ElementTree(file=weather_xml)
+    weather_root = weather_tree.getroot()
+    # current temp in F is located at 13. should make dynamic.
+    curr_temp = weather_root[13].text
+    # decimal precision is not necessary. strips to only whole number.
+    curr_temp = str(curr_temp).rpartition('.')[0]
+
+    # build string for temp
+    temp_str = 'The current temperature is {} degrees.'.format(curr_temp)
+    return str(temp_str)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
